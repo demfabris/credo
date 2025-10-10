38b84eda408d5e9f720ec8912fb3ed05714b19e0
Fabricio Dematte <demfabris@gmail.com>
2025-10-10 09:34:44 -0300

    ayu-dark blink

---
+++ .config/blink/ayu-dark.js
@@ -0,0 +1,23 @@
+// Ayu Dark theme for Blink Shell
+t.prefs_.set('color-palette-overrides', [
+	"#0A0E14", // black / background tone
+	"#F07178", // red
+	"#C2D94C", // green
+	"#FFB454", // yellow/orange
+	"#59C2FF", // blue
+	"#FF8F40", // magenta-ish (actually bright orange)
+	"#95E6CB", // cyan
+	"#CCCAC2", // white
+	"#01060E", // bright black
+	"#F07178", // bright red
+	"#C2D94C", // bright green
+	"#FFB454", // bright yellow
+	"#59C2FF", // bright blue
+	"#FF8F40", // bright magenta
+	"#95E6CB", // bright cyan
+	"#FFFDFB"  // bright white
+]);
+
+t.prefs_.set('foreground-color', "#CCCAC2");
+t.prefs_.set('background-color', "#0A0E14");
+t.prefs_.set('cursor-color', 'rgba(255,255,255,0.5)');
